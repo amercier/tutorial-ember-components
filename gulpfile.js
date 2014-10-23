34b0d05c3e5b1f54e04fb8cd71f6f52f5958b37b
Alexandre Mercier <amercier@slideshare.com>
2014-10-23 10:06:20 -0700

    Added minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var emberTemplates = require('gulp-ember-templates');
+var uglify = require('gulp-uglifyjs');
 
 gulp.task('default', function () {
   gulp.src('./templates/**/*.{handlebars,hbs}')
     .pipe(emberTemplates())
     .pipe(concat('templates.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('./js/dist'));
 });
