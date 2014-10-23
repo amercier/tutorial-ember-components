6f596f6c0bb68cceab4759ceedaef9abc35515a5
Alexandre Mercier <amercier@slideshare.com>
2014-10-23 11:23:10 -0700

    Factorized Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
+var path = require('path');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var emberTemplates = require('gulp-ember-templates');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
+
+/**
+ * Concatenates and uglify all `src` files into `dest`, including inline source
+ * maps.
+ * 
+ * @param {String}       dest Path of destination file
+ * @param {Array|String} src  Path of source file(s)
+ * 
+ * @returns {Pipe} Returns the Gulp pipe
+ */
+function uglifyAll(dest, src) {
+  return gulp.src(src)
+    .pipe(sourcemaps.init())
+    .pipe(concat(path.basename(dest)))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.dirname(dest)));
+}
 
 // .hbs -> .js
 gulp.task('views', function () {
@@ -15,33 +34,22 @@ gulp.task('views', function () {
 
 // *.js -> vendor.js
 gulp.task('vendor', function () {
-  return gulp.src([
+  return uglifyAll('dist/vendor.js', [
     'js/libs/jquery/dist/jquery.js',
     'js/libs/handlebars/handlebars.js',
     'js/libs/ember/ember.js',
     'js/libs/ember-data/ember-data.js',
     'js/libs/ember-localstorage-adapter/localstorage_adapter.js',
-  ])
-    .pipe(sourcemaps.init())
-    .pipe(concat('vendor.js'))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist'));
+  ]);
 });
-
 
 // *.js -> app.js
 gulp.task('app', ['views'], function () {
-  return gulp.src([
+  return uglifyAll('dist/app.js', [
     'dist/views/**',
     'js/application.js',
     'js/router.js'
-  ])
-    .pipe(sourcemaps.init())
-    .pipe(concat('app.js'))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist'));
+  ]);
 });
 
 // Default task
